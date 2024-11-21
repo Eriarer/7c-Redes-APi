335a06bfc9564d8b71117ff947a186b498b23528
eriarer <eriarer_messiah+git@hotmail.com>
2024-11-20 22:17:18 -0600

    add validation for updateUsuario route and enhance usuario schema error messages

--- routes/usuario.routes.js
+++ routes/usuario.routes.js
@@ -1,6 +1,9 @@
 import express from 'express'
 import { validateSchema } from '../middleware/validateSchema.middleware.js'
-import { addUsuarioSchema } from '../schema/usuario.schema.js'
+import {
+  addUsuarioSchema,
+  updateUsuarioSchema
+} from '../schema/usuario.schema.js'
 import {
   addUsuario,
   deleteUsuario,
@@ -18,7 +21,7 @@ router.post('/', validateSchema(addUsuarioSchema), addUsuario)
 router.delete('/:id', deleteUsuario)
 router.delete('/', deleteUsuarios)
 
-router.patch('/:id', updateUsuario)
+router.patch('/:id', validateSchema(updateUsuario), updateUsuario)
 
 router.get('/find', getUsuarios)
 router.get('/query', getUsuariosFiltered)
