095082355d83b2826feb1429a6031ebe78281c77
Eriarer <eriarer_melgoza+git@hotmail.com>
2024-11-21 10:22:47 -0600

    add getResponsables endpoint and route for fetching responsables

--- controllers/responsable.controller.js
+++ controllers/responsable.controller.js
@@ -5,6 +5,15 @@ const db = new JsonDatabase(appConfig.dbDirectory)
 await db.init()
 await db.createCollection('usuarios')
 
+export const getResponsables = async (req, res) => {
+  try {
+    const responsables = await db.queryDocuments('responsables')
+    res.status(200).json({ status: 'success', data: responsables })
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+}
+
 export const addResponsable = async (req, res) => {
   const { id, nombre, tipo } = req.body
   try {
--- routes/responsalbe.routes.js
+++ routes/responsalbe.routes.js
@@ -1,10 +1,14 @@
 import { Router } from 'express'
-import { addResponsable } from '../controllers/responsable.controller.js'
+import {
+  getResponsables,
+  addResponsable
+} from '../controllers/responsable.controller.js'
 import { addResponsableSchema } from '../schema/responsable.schema.js'
 import { validateSchema } from '../middleware/validateSchema.middleware.js'
 
 const router = Router()
 
 router.post('/create', validateSchema(addResponsableSchema), addResponsable)
+router.get('/find', getResponsables)
 
 export default router
