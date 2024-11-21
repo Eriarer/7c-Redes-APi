11853f0592ddb9f02a1ab12223ab4a8d9c9d0418
eriarer <eriarer_messiah+git@hotmail.com>
2024-11-20 22:54:53 -0600

    add additional schema validation middleware for request parameters and query

--- middleware/validateSchema.middleware.js
+++ middleware/validateSchema.middleware.js
@@ -1,8 +1,32 @@
 export const validateSchema = (schema) => (req, res, next) => {
-    try {
-        schema.parse(req.body)
-        next()
-    } catch (error) {
-        return res.status(400).json({ error: error.errors.map((error) => error.message) })
-    }
+  try {
+    schema.parse(req.body)
+    next()
+  } catch (error) {
+    return res
+      .status(400)
+      .json({ error: error.errors.map((error) => error.message) })
+  }
+}
+
+export const validateSchemaWithParams = (schema) => (req, res, next) => {
+  try {
+    schema.parse(req.params)
+    next()
+  } catch (error) {
+    return res
+      .status(400)
+      .json({ error: error.errors.map((error) => error.message) })
+  }
+}
+
+export const validateSchemaWithQuery = (schema) => (req, res, next) => {
+  try {
+    schema.parse(req.query)
+    next()
+  } catch (error) {
+    return res
+      .status(400)
+      .json({ error: error.errors.map((error) => error.message) })
+  }
 }
