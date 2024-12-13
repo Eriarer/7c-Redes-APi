b1d15acb17ba27d4416d6169fe47568bb9a490a3
eriarer <eriarer_messiah+git@hotmail.com>
2024-12-12 21:02:34 -0600

    refactor validateSchema middleware to improve error handling and response structure

--- middleware/validateSchema.middleware.js
+++ middleware/validateSchema.middleware.js
@@ -1,32 +1,22 @@
-export const validateSchema = (schema) => (req, res, next) => {
-  try {
-    schema.parse(req.body)
-    next()
-  } catch (error) {
-    return res
-      .status(400)
-      .json({ error: error.errors.map((error) => error.message) })
-  }
-}
+import { z } from 'zod'
 
-export const validateSchemaWithParams = (schema) => (req, res, next) => {
-  try {
-    schema.parse(req.params)
-    next()
-  } catch (error) {
-    return res
-      .status(400)
-      .json({ error: error.errors.map((error) => error.message) })
-  }
-}
-
-export const validateSchemaWithQuery = (schema) => (req, res, next) => {
-  try {
-    schema.parse(req.query)
-    next()
-  } catch (error) {
-    return res
-      .status(400)
-      .json({ error: error.errors.map((error) => error.message) })
+export const validateSchema = (schema) => {
+  return (req, res, next) => {
+    try {
+      req.body = schema.parse(req.body)
+      next()
+    } catch (error) {
+      if (error instanceof z.ZodError) {
+        const errorMessages = error.errors.map((err) => ({
+          campo: err.path[0],
+          message: err.message
+        }))
+        return res.status(400).json({
+          error: 'Validation failed',
+          details: errorMessages
+        })
+      }
+      res.status(500).json({ error: 'Internal server error' })
+    }
   }
 }
